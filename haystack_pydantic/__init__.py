31172c0d701f2ca87529b6406129254b2a54b9b7
Satoshi <6world4trigger@gmail.com>
2025-01-23 07:45:38 +0900

    add exports to __all__

--- haystack_pydantic/__init__.py
+++ haystack_pydantic/__init__.py
@@ -1,2 +1,4 @@
 from haystack_pydantic.core.pipeline.pipeline import PydanticWrappedPipeline as Pipeline
 from haystack_pydantic.core.component import component
+
+__all__ = ["Pipeline", "component"]
